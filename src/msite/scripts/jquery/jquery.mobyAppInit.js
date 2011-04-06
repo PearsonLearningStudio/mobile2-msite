7dd799bd14058fe5ea9b2e165909be58db67114c
kevin.bauman <kevin.bauman@effectiveui.com>
2011-04-06 14:00:40 -0600

    Fixed list of classes on profile page to not be a list of links, but simply a list of classes.
    Signed-off-by: kevin.bauman <kevin.bauman@effectiveui.com>

--- src/msite/scripts/jquery/jquery.mobyAppInit.js
+++ src/msite/scripts/jquery/jquery.mobyAppInit.js
@@ -330,8 +330,9 @@ boolClicked = true;
 											$items.show();
 											$(".view-discussion .mobi-listview .ui-li-divider:visible:first").addClass("ui-corner-top");
 										//}
-										
 									}
+									//trying to prevent page refresh on Pre 
+									return false;
 								})
 							}
 						});
@@ -796,6 +797,7 @@ boolClicked = true;
 				var courses = '', i, $classesList = $this.find('.view-courses .mobi-listview');
 				$().mobyCourseManager( {
 					callbackSuccess: function(arrCourses) { 
+						
 						$(arrCourses).each(function(i) { 
 							courses +='<li class="course">';
 							courses +='<a id="' + i + '"  href="/course.html" class="listitem-course">';
@@ -806,9 +808,12 @@ boolClicked = true;
 						} ); 
 						$classesList.html(courses);
 						$classesList.listview('refresh');
-						$classesList.find('.listitem-course').click(function() {
+						$classesList.find('.listitem-course').click(function(e) {
 							//add the current course data into the arrGlobalCourse array for #pageCourseDetail
 							varGlobalCourse = arrCourses[this.id];
+							if($this[0].id === 'pageProfile') {
+								return false;
+							}
 						} );
 						$.mobile.pageLoading(true);
 					},
